7e73e829fd664f365d57cc97b21ec56a0ca1fc29
brock elmore <brock.elmore@gmail.com>
2023-03-28 11:31:01 -0700

    remove unused fnt

--- src/lib.rs
+++ src/lib.rs
@@ -682,30 +682,3 @@ impl Analyzer {
         TyNode(self.add_node(ty).index())
     }
 }
-
-pub fn normalize_path(path: &Path) -> PathBuf {
-    let mut components = path.components().peekable();
-    let mut ret = if let Some(c @ Component::Prefix(..)) = components.peek().cloned() {
-        components.next();
-        PathBuf::from(c.as_os_str())
-    } else {
-        PathBuf::new()
-    };
-
-    for component in components {
-        match component {
-            Component::Prefix(..) => unreachable!(),
-            Component::RootDir => {
-                ret.push(component.as_os_str());
-            }
-            Component::CurDir => {}
-            Component::ParentDir => {
-                ret.pop();
-            }
-            Component::Normal(c) => {
-                ret.push(c);
-            }
-        }
-    }
-    ret
-}
