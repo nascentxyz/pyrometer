891f00b54017d4c1aa55cfae31eb72a2d8ba7064
brock elmore <brock.elmore@gmail.com>
2024-08-08 16:16:51 -1000

    reenable test

--- crates/pyrometer/tests/test_data/remapping_import.sol
+++ crates/pyrometer/tests/test_data/remapping_import.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.0;
 
 import "@relative/relative_import.sol";
 
-// contract RemappingImport {
-//     function deploy() public {
-//         Env env = Env(address(100));
-//         env.msg_sender();
-//     }
-// }
+contract RemappingImport {
+    function deploy() public {
+        Env env = Env(address(100));
+        env.msg_sender();
+    }
+}
